23c4598a4633942e80d4a0e3fa9270cde4837523
aaltmark <adamaltmark@gmail.com>
2020-08-18 22:24:48 -0400

    some edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
             token = encode_token({ user_id: @user.id })
             render json: { user: UserSerializer.new(@user), jwt: token }, status: :created 
         else 
-            render json: { error: 'failed to create user' }, status: :not_acceptable
+            render json: { error: 'Please fill out all required fields' }, status: :not_acceptable
         end
     end
 
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
         if user.valid?
             render json: user
         else
-            render json: { error: 'invalid user' }
+            render json: { error: 'Please fill out all required fields' }
         end 
     end
 
